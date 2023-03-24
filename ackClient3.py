9d04d509b831d5a5e5deed8ae8f23f08d23618c7
Bryan Fraschetti <74269342+bryanfraschetti@users.noreply.github.com>
2023-03-24 12:49:52 -0400

    Add files via upload
    pretty functional

--- ackClient3.py
+++ ackClient3.py
@@ -9,34 +9,64 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('localhost', 10000)
 
 base = 0
-N = 2
+N = 5
 nxtSeqNum = 0
 message = ['0','1','2','3','4','5','6','7','8','9']
 ackNum = 0
 
+skip4 = True #drop packet 4 on first transmission
+# skip8 = False #drop packet 8 during retransmission
+# skip0 = True #drop the first packet
+
 try:
-	while(base < len(message) - 1):
+	while(base < len(message)):
 		while nxtSeqNum <= base + N - 1:
-			sys.stderr.write('sending "%s"' % message[nxtSeqNum])
-			sock.sendto(message[nxtSeqNum].encode(), server_address)
-			nxtSeqNum +=1
+			if(nxtSeqNum < len(message)):
+				sys.stderr.write('\nsending "%s"' % message[nxtSeqNum])
+
+				# if(nxtSeqNum == 4 and skip4):
+				# 	nxtSeqNum = nxtSeqNum + 1
+				# 	skip4 = False
+				# 	continue
+				
+				# if(nxtSeqNum == 4 and not skip4):
+				# 	skip8 = True
+
+				# if(nxtSeqNum == 8 and skip8 and not skip4):
+				# 	nxtSeqNum = nxtSeqNum + 1
+				# 	skip8 = False
+				# 	continue
+				
+				# if(nxtSeqNum == 0 and skip0):
+				# 	nxtSeqNum = nxtSeqNum + 1
+				# 	skip0 = False
+				# 	continue
+				
+
+				sock.sendto(message[nxtSeqNum].encode(), server_address)
+				nxtSeqNum +=1
+			else:
+				break
 
 		sys.stderr.write('\nwaiting to receive ack')
-		
-		while (ackNum<base + N - 1):
-			ackNum, server = sock.recvfrom(4096)
-			ackNum = int(ackNum.decode())
-			sys.stderr.write('\nReceived ack ' + str(ackNum))
+				
+		sock.settimeout(1)
+		while(ackNum <= base + N - 1):
+			try:
+				ackNum, server = sock.recvfrom(4096)
+				ackNum = int(ackNum.decode())
+				print("\nReceived Ack " + str(ackNum))
+				
+			except:
+				nxtSeqNum = ackNum + 1
+				break
 
-		base = ackNum
-	
-
-
+		base = ackNum + 1
 
 finally:
 	sys.stderr.write('\nclosing socket')
-		
-	
+
+
 
 sock.close()
 
--- ackServer3.py
+++ ackServer3.py
@@ -10,30 +10,44 @@ sys.stderr.write('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 base = 0
-N = 2
+N = 5
 expSeqNum = 0
-
 collectedPackets = ''
+
+
+# skip3 = True # lose an ack
 
 while True:
     print("\n" + collectedPackets)
-    sys.stderr.write('\nwaiting to receive message')
+    sys.stderr.write('\nWaiting to receive message')
     
-#    while(int(data.decode()) < base + N - 1):
-
     data, address = sock.recvfrom(4096)
 
-    sys.stderr.write('\nreceived %s bytes from %s' % (len(data), address))
+    sys.stderr.write('\nReceived %s bytes from %s' % (len(data), address))
     sys.stderr.write("\n" + data.decode())
 
     try:
         data.decode()
         if int(data.decode()) == expSeqNum:
+
+            # #lose an ack
+            # if(expSeqNum != 3 and skip3):
+            #     sock.sendto(str(expSeqNum).encode(), address)
+            #     print("\nSent ack " + str(expSeqNum))
+            #     collectedPackets += str(expSeqNum) + " "
+            #     expSeqNum += 1
+            # else:
+            #     skip3 = False
+            #     collectedPackets += str(expSeqNum) + " "
+            #     expSeqNum += 1
+
             sock.sendto(str(expSeqNum).encode(), address)
+            print("\nSent ack " + str(expSeqNum))
             collectedPackets += str(expSeqNum) + " "
             expSeqNum += 1
+
         else:
-            sys.stderr.write("\nReceived " + str(len(data)) + "bytes from " + str(address) + ". Discarding packet, sending ACK " + str(expSeqNum-1) + ".")
+            sys.stderr.write("\nReceived " + str(len(data)) + " bytes from " + str(address) + ". Discarding packet... sending ack " + str(expSeqNum-1) + ".")
             sent = sock.sendto(str(expSeqNum-1).encode(), address)
     except:
         print(data.decode())
